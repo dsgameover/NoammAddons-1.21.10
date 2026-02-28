223fefce7c0a9a440a135d8e3312592d5f88e06b
Yennekar <asiancat414@gmail.com>
2026-02-27 23:57:35 -0500

    option to disable block overlay while using etherwarp

--- src/main/kotlin/com/github/noamm9/features/impl/misc/BlockOverlay.kt
+++ src/main/kotlin/com/github/noamm9/features/impl/misc/BlockOverlay.kt
@@ -1,6 +1,7 @@
 package com.github.noamm9.features.impl.misc
 
 import com.github.noamm9.features.Feature
+import com.github.noamm9.features.impl.general.teleport.EtherwarpHelper
 import com.github.noamm9.ui.clickgui.components.getValue
 import com.github.noamm9.ui.clickgui.components.hideIf
 import com.github.noamm9.ui.clickgui.components.impl.ColorSetting
@@ -11,6 +12,8 @@ import com.github.noamm9.ui.clickgui.components.provideDelegate
 import com.github.noamm9.utils.ColorUtils.withAlpha
 import com.github.noamm9.utils.Utils
 import com.github.noamm9.utils.Utils.equalsOneOf
+import com.github.noamm9.utils.items.ItemUtils.skyblockId
+import com.github.noamm9.utils.location.LocationUtils
 import com.github.noamm9.utils.render.Render3D
 import com.github.noamm9.utils.render.RenderContext
 import net.fabricmc.fabric.api.client.rendering.v1.world.WorldRenderContext
@@ -23,6 +26,7 @@ object BlockOverlay: Feature() {
     private val outlineColor by ColorSetting("Outline Color", Utils.favoriteColor, false).hideIf { mode.value == 1 }
     private val lineWidth by SliderSetting("Line Width", 2.5, 1, 10, 0.1).hideIf { mode.value == 1 }
     private val phase by ToggleSetting("Phase")
+    private val hideDuringEtherwarp by ToggleSetting("Hide while using Etherwarp")
 
     override fun init() {
         WorldRenderEvents.BEFORE_BLOCK_OUTLINE.register { context, blockOutlineContext ->
@@ -32,8 +36,17 @@ object BlockOverlay: Feature() {
         }
     }
 
+    private fun isEtherwarpOverlayLikelyActive(): Boolean {
+        val player = mc.player ?: return false
+        if (!player.isCrouching) return false
+
+        val held = player.mainHandItem.takeUnless { it.isEmpty } ?: return false
+        return EtherwarpHelper.getEtherwarpDistance(held) != null
+    }
+
     fun render(ctx: WorldRenderContext, blockCtx: BlockOutlineRenderState) {
         if (mc.options.hideGui) return
+        if (hideDuringEtherwarp.value && isEtherwarpOverlayLikelyActive()) return
 
         Render3D.renderBlock(
             RenderContext.fromContext(ctx),
